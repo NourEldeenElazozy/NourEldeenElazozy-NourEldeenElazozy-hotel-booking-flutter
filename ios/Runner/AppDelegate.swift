a9555feb0d8bd356ae7a8155f12c0a4df324b3c5
اسم المستخدم < >
2025-11-19 13:27:06 +0200

    with out maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Flutter
-import GoogleMaps
+//import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAJU7Qd9F9kj18nMnXSNB3Vodf0r4fhHOY")
+  //  GMSServices.provideAPIKey("AIzaSyAJU7Qd9F9kj18nMnXSNB3Vodf0r4fhHOY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
