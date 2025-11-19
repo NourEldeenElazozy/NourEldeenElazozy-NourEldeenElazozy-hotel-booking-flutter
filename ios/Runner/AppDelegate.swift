a52efc3a8ecb8e7dddd08026d7f68fd66258a61d
اسم المستخدم < >
2025-11-19 21:56:29 +0200

    with out maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
   //  GMSServices.provideAPIKey("AIzaSyAJU7Qd9F9kj18nMnXSNB3Vodf0r4fhHOY")
+  GMSServices.provideAPIKey("AIzaSyDC9WFXg8tjm5UlquX9IVSb2Mkv1wiQjFk")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
