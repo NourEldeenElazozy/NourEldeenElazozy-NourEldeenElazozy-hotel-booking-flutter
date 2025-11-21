06c0d622932ed7fff120eda30fb4ffd966e08578
اسم المستخدم < >
2025-11-21 03:50:59 +0200

    final ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,14 @@
 import UIKit
 import Flutter
-import GoogleMaps
-import FirebaseCore
+//import GoogleMaps
+//import FirebaseCore
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDC9WFXg8tjm5UlquX9IVSb2Mkv1wiQjFk")
+    //GMSServices.provideAPIKey("AIzaSyDC9WFXg8tjm5UlquX9IVSb2Mkv1wiQjFk")
       //FirebaseApp.configure()
 
 
